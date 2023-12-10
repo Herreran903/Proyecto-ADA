c31721a3f87d68ffed854f735ac4da734b5e4580
Samuel Galindo <itzsaky@outlook.com>
2023-12-09 19:07:31 -0500

    Pilas y escena2 terminado

--- Estructuras/Escena2.py
+++ Estructuras/Escena2.py
@@ -4,11 +4,5 @@ from Modelos.Pila import Pila
 class Escena2:
     def __init__(self, pila: Pila):
         self.pila = pila
-        self.grandeza = pila.getGrandeza()
-        self.maxGrandeza = pila.maxGrandeza()
-    
-    def getPila(self):
-        return self.pila
-    
-    def mayorGrandeza(self):
-        return self.pila.getGrandeza()
+        self.grandeza = pila.GrandezaTotal()
+        self.maxGrandeza = pila.mayorGrandeza()
