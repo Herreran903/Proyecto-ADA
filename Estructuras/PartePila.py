38e837d9bf757280ddefeea2e85981307543d9e7
Herdavid03 <115738297+Herdavid03@users.noreply.github.com>
2023-12-15 21:11:56 -0500

    Update PartePila.py

--- Estructuras/PartePila.py
+++ Estructuras/PartePila.py
@@ -13,3 +13,22 @@ class PartePila:
     
     def getPila(self):
         return self.pila
+
+    def buildPila(self):
+        self.pila.sort(key=lambda escenario_pila: escenario_pila.grandezaTotal)
+
+    def sumarGrandezas(self):
+        return sum(escenario_pila.grandezaTotal for escenario_pila in self.pila)
+
+    def getGrandezaMinima(self):
+        return min(escenario_pila.grandezaTotal for escenario_pila in self.pila)
+
+    def getGrandezaMaxima(self):
+        return max(escenario_pila.grandezaTotal for escenario_pila in self.pila)
+
+    def getEscenarioMayorGrandeza(self):
+        return max(self.pila, key=lambda escenario_pila: escenario_pila.grandezaTotal)
+
+    def getEscenarioMenorGrandeza(self):
+        return min(self.pila, key=lambda escenario_pila: escenario_pila.grandezaTotal)
+        
